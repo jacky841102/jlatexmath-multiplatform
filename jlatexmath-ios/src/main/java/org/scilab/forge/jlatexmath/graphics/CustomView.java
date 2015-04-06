40224833285f0aa2bf1dcdfcf8abfd73b0f41d8d
jacky841102 <jacky841102@gmail.com>
2015-04-06 10:47:18 +0800

    Update CustomView.java

--- jlatexmath-ios/src/main/java/org/scilab/forge/jlatexmath/graphics/CustomView.java
+++ jlatexmath-ios/src/main/java/org/scilab/forge/jlatexmath/graphics/CustomView.java
@@ -23,29 +23,22 @@ public class CustomView extends UIView{
 	public CustomView(CGRect frame){
 		super(frame);
 		setBackgroundColor(UIColor.lightGray());
-		//CGContext context = UIGraphics.getCurrentContext();
-		//System.out.println("context == null: " + (context==null));
 	}
 	
 	@Override
 	public void draw(CGRect rect){
 		mCGContext = UIGraphics.getCurrentContext();
-		//System.out.println("mCGContext == null: " + (mCGContext==null));
 		String latex = "\\sqrt{1+\\frac{a}{b}}";
-		
-        TeXFormula formula = new TeXFormula(latex);
-        TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, 50);
-        icon.setInsets(new Insets(5, 5, 5, 5));
-        ImageI image = new ImageI();
-        Graphics2DInterface g2 = image.createGraphics2D(mCGContext, new CGRect(100,100,200,200));
-        g2.setColor(ColorUtil.WHITE);
-        //g2.fillRect(100, 100, icon.getIconWidth(), icon.getIconHeight());
-        icon.paintIcon(null, g2, 100, 100);
+        	TeXFormula formula = new TeXFormula(latex);
+        	TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, 50);
+        	icon.setInsets(new Insets(5, 5, 5, 5));
+        	ImageI image = new ImageI();
+        	Graphics2DInterface g2 = image.createGraphics2D(mCGContext, new CGRect(100,100,200,200));
+        	g2.setColor(ColorUtil.WHITE);
+        	icon.paintIcon(null, g2, 100, 100);
 		mCGContext.setLineWidth(2.0);
 		mCGContext.setStrokeColor(UIColor.black().getCGColor());
-        //mCGContext.addArc(50, 100, 20.0, 0.0, 6.28, 0);
-        mCGContext.strokePath();
-		//mCGContext = UIGraphics.getCurrentContext();
+        	mCGContext.strokePath();
 	}
 	
 	public void drawString(CGRect rect, String string, int x, int y, UIFont font){
